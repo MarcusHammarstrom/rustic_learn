c546fda6cc527e6c20b518b128b5e81249c07bac
MarcusHammarstrom <marham1995@gmail.com>
2025-06-08 21:07:15 +0200

    added chebyshev distance

--- src/metrics/distance_functions.rs
+++ src/metrics/distance_functions.rs
@@ -10,8 +10,18 @@ pub fn manhattan_distance(a: &Vec<f32>, b: &Vec<f32>) -> f32 {
     assert_eq!(a.len(), b.len(), "Vectors must be of the same length");
     let mut distance = 0.0;
     for (x, y) in a.iter().zip(b.iter()) {
-        let value = x - y;
-        distance += value.abs();
+        distance += (x-y).abs();
     }
     distance
+}
+pub fn chebyshev_distance(a: &Vec<f32>, b: &Vec<f32>) -> f32 {
+    assert_eq!(a.len(), b.len(), "Vectors must be of the same length");
+    let mut max = 0.0;
+    for (x, y) in a.iter().zip(b.iter()) {
+        let value = (x-y).abs();
+        if value > max {
+            max = value;
+        }
+    }
+    max
 }
