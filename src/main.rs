31e695666139e36aed279feeb9d4db2d244523f2
MarcusHammarstrom <marham1995@gmail.com>
2025-06-10 20:18:56 +0200

    Ordinary linear regression implemented

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rustic_learn::classifiers::KnnClassifier;
+use rustic_learn::regressors::LinearRegression;
 use rustic_learn::datasets::{load_boston_housing, load_iris};
 use rustic_learn::model_selection::train_test_split;
 
@@ -18,6 +19,17 @@ fn main() {
     let accuracy = correct / total as f64;
     println!("Accuracy: {:.2}%", accuracy * 100.0);
     let (x, y) = load_boston_housing();
-    println!("Loaded Boston Housing dataset with {} samples and {} features.", x.len(), x[0].len());
-    println!("{y:?}");
+    let (x_train, y_train, x_test, y_test) = train_test_split(&x, &y, Some(0.2), None);
+    let mut regressor = LinearRegression::new();
+    regressor.fit(&x_train, &y_train);
+    let predictions = regressor.predict(&x_test);
+    let mut rmse = 0.0;
+    for (predicted, actual) in predictions.iter().zip(y_test.iter()) {
+        rmse += (predicted - actual).powi(2);
+    }
+    rmse = rmse / predictions.len() as f64;
+    rmse = rmse.sqrt();
+    println!("Root Mean Squared Error: {:.2}", rmse);
+    println!("Prediction 1: {}, Actual 1: {}", predictions[0], y_test[0]);
+    println!("Prediction 2: {}, Actual 1: {}", predictions[1], y_test[1]);   
 }
